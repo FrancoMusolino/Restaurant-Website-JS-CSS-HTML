f1a8fd86e4badd368eecda78ddeb02ba013a9755
FrancoMusolino <francomusolino55@gmail.com>
2021-05-01 12:57:14 -0300

    Test

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,5 +50,67 @@ function scrollActive() {
 window.addEventListener('scroll', scrollActive);
 
 
+// Change Background Header
+
+function scrollHeader() {
+    const nav = document.getElementById('header')
+
+    if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
 
 
+//Scroll Top
+
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top')
+
+    if (this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollTop);
+
+
+//DARK/LIGHT MODE
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+});
+
+
+//Scroll Reveal Animation
+
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '30px',
+    duration: 1000,
+    reset: true
+});
+
+sr.reveal(`.home__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content,
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`, {
+    interval: 200
+})
