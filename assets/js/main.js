900a6c06ee1c902517b0464cdfd30a71debdac3f
FrancoMusolino <francomusolino55@gmail.com>
2021-04-30 21:12:56 -0300

    Scrool Behavior Header

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+//Show Menu (Press the bars)
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu');
+
+
+// Remove Menu by navLinks
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+// Scroll Section
+
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
+
+
+
+
