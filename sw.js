49883b5e69bfa44c8861181fee76ae35bb4a8d3e
FrancoMusolino <francomusolino55@gmail.com>
2021-08-09 11:34:33 -0300

    Testing V4

--- sw.js
+++ sw.js
@@ -27,25 +27,12 @@ self.addEventListener('install', e => {
     )
 })
 
-const addAllToCache = () => {
-    caches.open(CACHE_NAME)
-        .then(cache => {
-            //Una vez que hayas abierto el cache con ese nombre, añadi todas estas Urls a ese cache
-            return cache.addAll(urlsToCache)
-                //Evitamos la espera, se activo ni bien termina la instalación
-                .then(() => self.skipWaiting())
-        })
-        .catch(err => console.log(err))
-}
-
-document.addEventListener('DOMContentLoaded', addAllToCache());
 
 //una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
 self.addEventListener('activate', e => {
     const cacheWhitelist = [CACHE_NAME]
 
     e.waitUntil(
-
         caches.keys()
             .then(cacheNames => {
                 return Promise.all(
@@ -65,7 +52,6 @@ self.addEventListener('activate', e => {
 
 //Cuando el navegador recupera una url
 self.addEventListener('fetch', e => {
-    //Responder ya sea con el objeto en caché o continuar y buscar la url real
     e.respondWith(
         caches.match(e.request)
             .then(res => {
