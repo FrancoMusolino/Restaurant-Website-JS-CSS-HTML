46f2b9e715e0662d3c8874f4f6f2b1ef5eaf6e8d
FrancoMusolino <francomusolino55@gmail.com>
2021-08-25 00:25:53 -0300

    Pwa Offline

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'v1_cache_restaurant_website',
+const CACHE_NAME = 'v1_cache_restaurant',
     urlsToCache = [
         './',
-        'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap',
-        'https://fonts.gstatic.com/s/poppins/v15/pxiEyp8kv8JHgFVrJJbecmNE.woff2',
-        'https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css',
-        'https://cdn.jsdelivr.net/npm/boxicons@2.0.5/fonts/boxicons.woff2',
-        './assets/css/style.css',
+        './assets/css/styles.css',
         './assets/js/main.js',
         './assets/img/plate1.png',
         './assets/img/plate2.png',
-        './assets/img/favicon.ico'
+        './assets/img/favicon.ico',
+        'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap',
+        'https://fonts.gstatic.com/s/poppins/v15/pxiEyp8kv8JHgFVrJJbecmNE.woff2',
+        'https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css',
+        'https://unpkg.com/boxicons@2.0.9/fonts/boxicons.woff',
     ]
 
 //Se instala el Service Worker, pasa solo 1 vez
@@ -23,7 +23,7 @@ self.addEventListener('install', e => {
                     //Evitamos la espera, se activo ni bien termina la instalación
                     .then(() => self.skipWaiting())
             })
-            .catch(err => console.log(err))
+        // .catch(err => console.error(err))
     )
 })
 
@@ -38,8 +38,8 @@ self.addEventListener('activate', e => {
                 return Promise.all(
                     cacheNames.map(cacheName => {
                         //Eliminamos lo que ya no se necesita en cache
-                        if (cacheWhitelist.indexOf(cacheName) === -1) {
-                            return caches.delete(cacheName)
+                        if (!cacheWhitelist.includes(cacheName)) {
+                            return caches.delete(cacheName);
                         }
                     })
                 )
